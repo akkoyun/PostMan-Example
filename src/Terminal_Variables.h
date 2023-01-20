578c02a76d27629736988151cc8aff24b5e8480a
Mehmet Günce Akkoyun <akkoyun@me.com>
2023-01-20 10:49:23 +0300

    First upload

---
+++ src/Terminal_Variables.h
@@ -0,0 +1,48 @@
+// Terminal Coordinates
+#define GSM_Console_Initialize_X	(uint8_t) 5
+#define GSM_Console_Initialize_Y 	(uint8_t) 4
+
+#define GSM_Console_Connect_X 		(uint8_t) 5
+#define GSM_Console_Connect_Y 		(uint8_t) 42
+
+#define GSM_Console_Boot_X 			(uint8_t) 29
+#define GSM_Console_Boot_Y 			(uint8_t) 10
+
+#define GSM_Manufacturer_X 			(uint8_t) 5
+#define GSM_Manufacturer_Y 			(uint8_t) 116
+
+#define GSM_Model_X 				(uint8_t) 6
+#define GSM_Model_Y		 			(uint8_t) 116
+
+#define GSM_Firmware_X 				(uint8_t) 7
+#define GSM_Firmware_Y	 			(uint8_t) 108
+
+#define GSM_IMEI_X 					(uint8_t) 8
+#define GSM_IMEI_Y		 			(uint8_t) 102
+
+#define GSM_Serial_X 				(uint8_t) 9
+#define GSM_Serial_Y		 		(uint8_t) 107
+
+#define GSM_ICCID_X 				(uint8_t) 10
+#define GSM_ICCID_Y			 		(uint8_t) 98
+
+#define GSM_ConnTime_X 				(uint8_t) 13
+#define GSM_ConnTime_Y		 		(uint8_t) 113
+
+#define GSM_RSSI_X 					(uint8_t) 14
+#define GSM_RSSI_Y			 		(uint8_t) 115
+
+#define GSM_Operator_X 				(uint8_t) 15
+#define GSM_Operator_Y		 		(uint8_t) 112
+
+#define GSM_CellID_X 				(uint8_t) 18
+#define GSM_CellID_Y		 		(uint8_t) 113
+
+#define GSM_CellLAC_X 				(uint8_t) 17
+#define GSM_CellLAC_Y		 		(uint8_t) 113
+
+#define GSM_IP_X 					(uint8_t) 16
+#define GSM_IP_Y			 		(uint8_t) 102
+
+#define GSM_PostOfficeStatus_X 		(uint8_t) 26
+#define GSM_PostOfficeStatus_Y		(uint8_t) 4
